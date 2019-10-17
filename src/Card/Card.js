8f411309423c2509134010655a348b03700e3e94
dawnlunacy <rudd.lacy@gmail.com>
2019-10-17 09:50:50 -0600

    Add basic styling to card

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import './Card.css'
 
 const Card = () => {
     return (
-        <p> Testing our communication from Card</p>
+        <article className="card">
+            <h3>  name </h3>
+            <h4> Date </h4>
+            <h4> time </h4>
+            <h4> # of guests </h4>
+            <button className="cancel-btn"> Cancel </button>
+        </article>
     )
 }
 
