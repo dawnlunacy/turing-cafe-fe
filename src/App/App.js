0a6cd0c5bad55464bc24d273125c9312743da522
dawnlunacy <rudd.lacy@gmail.com>
2019-10-17 09:33:45 -0600

    Add cardContainer and test communication

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import CardContainer from '../CardContainer/CardContainer'
 
 class App extends Component {
   render() {
@@ -7,10 +8,10 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+    
         </div>
         <div className='resy-container'>
-          
+          <CardContainer />
         </div>
       </div>
     )
---
+++ src/CardContainer/CardContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CardContainer = (props) => {
+    return (
+        <p> Testin our communication </p>
+    )
+}
+
+export default CardContainer;
