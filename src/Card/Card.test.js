4e5cb1d69870aa0168221b107331f929d98e2fef
dawnlunacy <rudd.lacy@gmail.com>
2019-10-17 12:03:13 -0600

    Add snapshot test for CardContainer:

--- src/Card/Card.test.js
+++ src/Card/Card.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import Card from './Card';
 
 describe('Card', () => {
-  it('should return a card display the correct reservation informatino', () => {
+  it('should return a card display the correct reservation information', () => {
     const wrapper = shallow(
       <Card />
     )
--- src/CardContainer/CardContainer.test.js
+++ src/CardContainer/CardContainer.test.js
@@ -1,3 +1,35 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import CardContainer from './CardContainer/CardContainer'
+import CardContainer from './CardContainer';
+
+describe('CardContainer', () => {
+  it('should return reservations cards matching snapshot', () => {
+    const mockReservations = [
+        {
+            "id": 1,
+            "name": "Christie",
+            "date": "12/29",
+            "time": "7:00",
+            "number": 12
+        },
+        {
+            "id": 2,
+            "name": "Leta",
+            "date": "4/5",
+            "time": "7:00",
+            "number": 2
+        },
+        {
+            "id": 3,
+            "name": "Pam",
+            "date": "1/21",
+            "time": "6:00",
+            "number": 4
+        }]
+    const wrapper = shallow(
+      <CardContainer reservations={mockReservations}/>
+    )
+    
+    expect(wrapper).toMatchSnapshot();
+  })
+})
